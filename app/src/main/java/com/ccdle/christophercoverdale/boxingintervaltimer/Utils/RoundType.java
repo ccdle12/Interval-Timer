c026e8d712517ed688fad33f292e6ab30e96e35b
ccdle12 <chris.coverdale24@gmail.com>
2017-08-10 21:32:16 +0100

    Updates

---
+++ app/src/main/java/com/ccdle/christophercoverdale/boxingintervaltimer/Utils/RoundType.java
@@ -0,0 +1,27 @@
+package com.ccdle.christophercoverdale.boxingintervaltimer.Utils;
+
+/**
+ * Created by christophercoverdale on 08/08/2017.
+ */
+
+public class RoundType
+{
+    private long roundTime;
+    private String roundType;
+
+    public RoundType(long roundTime, String roundType)
+    {
+        this.roundTime = roundTime;
+        this.roundType = roundType;
+    }
+
+    public String getRoundType()
+    {
+        return roundType;
+    }
+
+    public long getRoundTime()
+    {
+        return roundTime;
+    }
+}
